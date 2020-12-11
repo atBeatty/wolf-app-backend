cf1a48f0a847a0e593317d9e4adefc772f5243fb
atBeatty <adam.t.beatt@gmail.com>
2020-12-10 17:54:10 -0800

    publish

--- app/models/golfer.rb
+++ app/models/golfer.rb
@@ -2,4 +2,10 @@ class Golfer < ApplicationRecord
     belongs_to :foursome, optional: true
     has_many :courses, through: :foursome
 
+
+    def wolf_games
+        binding.pry
+        
+    end
+
 end
